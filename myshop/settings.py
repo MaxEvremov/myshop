9c0867a1792d50c811b085918203ceb359d59b6e
MaxEvremov <92534316+MaxEvremov@users.noreply.github.com>
2022-01-18 00:59:03 +0500

    ver 17.01.2022

--- myshop/settings.py
+++ myshop/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'templates')
+            os.path.join(BASE_DIR, 'templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -124,6 +124,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-namespace='shop'
+
 urlpatterns = [
-    path('', views.product_list),
+    path('', views.product_list, name='product_list'),
     path('<category_slug>', views.product_list, name='product_list_by_category'),
-    path('<id>/<slug>', views.product_detail, name='product_detail')
+    path('(<id>/<slug>)', views.product_detail, name='product_detail')
 ]
